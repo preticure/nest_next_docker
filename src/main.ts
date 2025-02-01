791165b4796fda86fab3ef23ebc1cf0f526b942d
yamaguchi aimi <preticure112771@gmail.com>
2025-02-02 05:44:08 +0900

    prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { Request } from "express";
+import * as cookieParser from "cookie-parser";
+import * as csurf from "csurf";
+
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+	app.enableCors({
+		credentials: true,
+		origin: "http://localhost:8080",
+	});
+	app.use(cookieParser());
 	await app.listen(3000);
 }
 bootstrap();
