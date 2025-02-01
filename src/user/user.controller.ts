b2498f7240411105bead9fddb0ab12f95b2374d1
yamaguchi aimi <preticure112771@gmail.com>
2025-02-02 07:57:57 +0900

    user_service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+import { Request } from 'express'
+import { User } from '@prisma/client'
 
+import { UserService } from './user.service'
+import { UpdateUserDto } from './dto/update-user.dto'
+
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getCurrentUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user
+  }
+
+  @Patch()
+  update(@Req() req: Request, @Body() dto: UpdateUserDto): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.update(req.user.id, dto)
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common'
+import { User } from '@prisma/client'
+
+import { PrismaService } from '../prisma/prisma.service'
+import { UpdateUserDto } from './dto/update-user.dto'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async update(id: number, dto: UpdateUserDto): Promise<Omit<User, 'hashedPassword'>> {
+    const user = await this.prisma.user.update({
+      where: { id },
+      data: {
+        ...dto,
+      },
+    })
+    user.hashedPassword = undefined
+
+    return user
+  }
+}
